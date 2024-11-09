28fcc22fa46c79c3b2de3fa2f000da33cb91456a
parthesh-tiwari <parthesh1000@gMAIL.COM>
2024-11-10 01:26:23 +0530

    adds/auth contexta and useAuth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,61 @@
+import api from "../../constants/api";
+
+const useAuth = () => {
+  const verifyAuth = () => {
+    try {
+    } catch (error) {
+      console.log("Error from useAuth's verifyAuth", error);
+      return false;
+    }
+  };
+
+  const register = async (payload) => {
+    try {
+      const data = JSON.stringify(payload);
+
+      const response = await api.post("/api/auth/sign-up", data);
+
+      if (response?.data) {
+        const data = response.data;
+
+        return data.message === "user-created"
+          ? { user: data.user, message: "User registeration successful" }
+          : { user: null, message: "User registeration failed" };
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log("Error from useAuth's register", error);
+      return false;
+    }
+  };
+
+  const login = async (payload) => {
+    try {
+      const data = JSON.stringify(payload);
+
+      const response = await api.post("/api/auth/login", data);
+
+      if (response?.data) {
+        const data = response.data;
+
+        return data.status === "success"
+          ? { user: data.user, message: data.status, token: data.token }
+          : { user: null, message: data.status, token: null };
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log("Error from useAuth's login", error);
+      return false;
+    }
+  };
+
+  return {
+    login,
+    register,
+    verifyAuth,
+  };
+};
+
+export default useAuth;
