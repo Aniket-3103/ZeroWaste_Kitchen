d16c931fc084fcfd739416b10e0c5a6909bc9a66
parthesh-tiwari <parthesh1000@gMAIL.COM>
2024-11-10 03:32:49 +0530

    integrates/auth in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from "react-toastify";
 import NavHome from "./navigation/NavHome";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <NavHome />;
+  return (
+    <>
+      <NavHome />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
