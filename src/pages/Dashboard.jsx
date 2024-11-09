6a76b3cf58be3b36ceda6fb3566970f41dc63342
shubs-code <shubs.code@gmail.com>
2024-11-09 19:28:33 +0530

    conten sectiono

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,9 +4,11 @@ function Dashboard(){
     return (
         <div className="flex justify-start h-screen w-full">
             <Sidebar />
-            <div className="md:ml-[calc(250px+4vh)] grow mr-[2vh]">
+            <div className="md:ml-[calc(250px+4vh)] grow flex flex-col mr-[2vh]">
                 <Header />
+                <div className="bg-blue-500 my-[2vh] rounded-2xl w-full h-full">
 
+                </div>
             </div>
         </div>
     )
