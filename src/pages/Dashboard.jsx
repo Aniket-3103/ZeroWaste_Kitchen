4fe14f309c5fddf9f6a44d829f271cad8ea708cd
shubs-code <shubs.code@gmail.com>
2024-11-10 08:27:04 +0530

    dashboard fix for sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ import Sidebar from "../components/Sidebar";
 function Dashboard(){
     return (
         <div className="flex justify-start min-h-screen w-full">
-            <div className="hidden md:visible"><Sidebar /></div>
+            <div className="hidden md:block"><Sidebar /></div>
             <div className="ml-[calc(2vh)] md:ml-[calc(250px+4vh)] grow flex flex-col mr-[2vh]">
                 <Header />
                 <div className=" my-[2vh] rounded-2xl w-full h-full">
